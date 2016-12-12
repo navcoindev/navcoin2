fd0b8a4c8d6a4ff8256c96c8e75311d81ca52774
sherlockcoin <sherlockcoin@gmail.com>
2016-12-12 17:40:09 +0530

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -100,10 +100,10 @@ public:
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,53);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,85);
-        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,150);
-        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_COIN_TYPE]  = list_of(0x80000082).convert_to_container<std::vector<unsigned char> >();             // NavCoin BIP44 coin type is '82' Index "130" 
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,150);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_COIN_TYPE]  = list_of(0x80000082).convert_to_container<std::vector<unsigned char> >();             // NavCoin BIP44 coin type is '82' Index "130"
         
         
 
